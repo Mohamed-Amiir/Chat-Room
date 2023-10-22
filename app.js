06bdf26d48e0f948b06c36b271e999213cafe0c1
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-10-22 21:43:47 +0300

    updates

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const http = require("http");
-const path = require("path");
-const { Server } = require("socket.io");
-const mongoose = require("mongoose");
-const userRouter = require("./routes/userRouter");
-const bodyParser = require("body-parser");
+const app = express(); // creat app
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http");
+const server = http.createServer(app); //creat HTTP server for our app
+
+const { Server } = require("socket.io");
+const io = new Server(server); // attach our HTTP app server with socket.io
+
+const userRouter = require("./routes/userRouter");
+app.use("/user", userRouter);
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 mongoose
   .connect("mongodb://localhost:27017/chat", {
@@ -34,13 +38,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/main/main.html"));
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+const privateChatRooms = {};
+
+io.on("connection", (client) => {
+  // Check if the user is already in a private chat room
+  const privateChatRoomId = privateChatRooms[client.id];
+  if (privateChatRoomId) {
+    client.join(privateChatRoomId);
+  } else {
+    // Create a new private chat room for the user
+    const newPrivateChatRoomId = Math.random().toString(36).substring(7);
+    privateChatRooms[client.id] = newPrivateChatRoomId;
+    client.join(newPrivateChatRoomId);
+  }
+
+  // Listen for private messages
+  client.on("private message", ({ message, to }) => {
+    // Emit the message to the recipient's private chat room
+    io.to(privateChatRooms[to]).emit("private message", message);
   });
 });
 
-app.use("/user", userRouter);
+
 
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
--- public/main/main.js
+++ public/main/main.js
@@ -1,20 +1,49 @@
-const socket = io();
-
+const client = io();
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 const messages = document.getElementById("messages");
+const recipientSelect = document.getElementById("recipient");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
-    socket.emit("chat message", input.value);
+    // Get the recipient of the private message
+    const recipient = recipientSelect.value;
+
+    // Emit the message to the recipient's private chat room
+    client.emit("private message", {
+      message: input.value,
+      to: recipient,
+    });
+
+    // Display the message on the sender's screen
+    displayMessage(input.value, 'sender');
+
+    // Clear the input field
     input.value = "";
   }
 });
 
-socket.on("chat message", (msg) => {
+client.on("private message", (msg) => {
+  // Display the message on the receiver's screen
+  displayMessage(msg, 'receiver');
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+function displayMessage(msg, className) {
   const item = document.createElement("li");
   item.textContent = msg;
+  item.classList.add(className);
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
+}
+
+// Populate the recipient select element with a list of all other users in the application
+client.on("users", (users) => {
+  users.forEach((user) => {
+    const option = document.createElement("option");
+    option.value = user;
+    option.textContent = user;
+    recipientSelect.appendChild(option);
+  });
 });
