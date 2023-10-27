1bc4f37dfbfe3814d60b7d739e857e1496911558
Mohamed Amir <83864018+Mohamed22198@users.noreply.github.com>
2023-10-28 00:06:31 +0200

    0

--- app.js
+++ app.js
@@ -1,65 +1,35 @@
 const express = require("express");
-const app = express(); // creat app
-
+const app = express();
 const http = require("http");
-const server = http.createServer(app); //creat HTTP server for our app
-
+const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server); // attach our HTTP app server with socket.io
+const io = new Server(server);
 
 const userRouter = require("./routes/userRouter");
-app.use("/user", userRouter);
-
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 const path = require("path");
 
-mongoose
-  .connect("mongodb://localhost:27017/chat", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+app.use("/user", userRouter);
 
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public/main")));
-
-// Use bodyParser middleware for parsing JSON and URL-encoded data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/main/main.html"));
 });
 
-const privateChatRooms = {};
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
-io.on("connection", (client) => {
-  // Check if the user is already in a private chat room
-  const privateChatRoomId = privateChatRooms[client.id];
-  if (privateChatRoomId) {
-    client.join(privateChatRoomId);
-  } else {
-    // Create a new private chat room for the user
-    const newPrivateChatRoomId = Math.random().toString(36).substring(7);
-    privateChatRooms[client.id] = newPrivateChatRoomId;
-    client.join(newPrivateChatRoomId);
-  }
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 
-  // Listen for private messages
-  client.on("private message", ({ message, to }) => {
-    // Emit the message to the recipient's private chat room
-    io.to(privateChatRooms[to]).emit("private message", message);
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
   });
 });
-
-
 
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
--- public/main/main.js
+++ public/main/main.js
@@ -1,49 +1,23 @@
-const client = io();
+const socket = io();
 const form = document.getElementById("form");
-const input = document.getElementById("input");
+const input = document.getElementById("message");
 const messages = document.getElementById("messages");
-const recipientSelect = document.getElementById("recipient");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (input.value) {
-    // Get the recipient of the private message
-    const recipient = recipientSelect.value;
+  
+  const message = input.value;
 
-    // Emit the message to the recipient's private chat room
-    client.emit("private message", {
-      message: input.value,
-      to: recipient,
-    });
+  // Emit the chat message to the server
+  socket.emit('chat message', message);
 
-    // Display the message on the sender's screen
-    displayMessage(input.value, 'sender');
-
-    // Clear the input field
-    input.value = "";
-  }
+  // Clear the input field
+  input.value = '';
 });
 
-client.on("private message", (msg) => {
-  // Display the message on the receiver's screen
-  displayMessage(msg, 'receiver');
-  window.scrollTo(0, document.body.scrollHeight);
-});
-
-function displayMessage(msg, className) {
-  const item = document.createElement("li");
-  item.textContent = msg;
-  item.classList.add(className);
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-}
-
-// Populate the recipient select element with a list of all other users in the application
-client.on("users", (users) => {
-  users.forEach((user) => {
-    const option = document.createElement("option");
-    option.value = user;
-    option.textContent = user;
-    recipientSelect.appendChild(option);
-  });
+// Listen for incoming chat messages from the server
+socket.on('chat message', (msg) => {
+  const messageElement = document.createElement('li');
+  messageElement.textContent = msg;
+  messages.appendChild(messageElement);
 });
